47f8f02891c62f89508ea9b4204f021f3dad7622
Malloy <li3210361050@gmail.com>
2025-07-28 04:02:57 +0800

    feat(icons): update some icon

---
+++ src/components/icons/Bubble.vue
@@ -0,0 +1,60 @@
+<!-- Bubble.vue -->
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 225 230" width="225"
+        height="230" preserveAspectRatio="xMidYMid meet"
+        style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;">
+        <defs>
+            <clipPath id="__lottie_element_14439">
+                <rect width="225" height="230" x="0" y="0"></rect>
+            </clipPath>
+            <clipPath id="__lottie_element_14444">
+                <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
+            </clipPath>
+            <clipPath id="__lottie_element_14451">
+                <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
+            </clipPath>
+            <clipPath id="__lottie_element_14458">
+                <path d="M0,0 L828,0 L828,1472 L0,1472z"></path>
+            </clipPath>
+        </defs>
+        <g clip-path="url(#__lottie_element_14439)">
+            <g clip-path="url(#__lottie_element_14458)"
+                transform="matrix(1.399999976158142,0,0,1.399999976158142,-101.0999755859375,-895.4000244140625)"
+                opacity="0.61" style="display: block;">
+                <g transform="matrix(0.1608973741531372,0,0,0.1608973741531372,129,779)" opacity="0.0012418094003157165"
+                    style="display: none;">
+                    <path class="progress-bar-particle-lottie" fill="rgb(255,200,0)" fill-opacity="1"
+                        d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z">
+                    </path>
+                </g>
+            </g>
+            <g clip-path="url(#__lottie_element_14451)" style="display: block;"
+                transform="matrix(-1.2852754592895508,0.6268706917762756,-0.6268706917762756,-1.2852754592895508,791.97119140625,934.405517578125)"
+                opacity="1">
+                <g transform="matrix(0.09744277596473694,0,0,0.09744277596473694,154,769)"
+                    opacity="0.041838740533597873" style="display: block;">
+                    <path class="progress-bar-particle-lottie" fill="rgb(255,200,0)" fill-opacity="1"
+                        d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z">
+                    </path>
+                </g>
+            </g>
+            <g clip-path="url(#__lottie_element_14444)"
+                transform="matrix(1.350000023841858,0,0,1.350000023841858,-89.4000244140625,-880.6000366210938)"
+                opacity="1" style="display: block;">
+                <g transform="matrix(-0.00917859748005867,0,0,-0.00917859748005867,77,797)"
+                    opacity="0.0011275092255877439" style="display: none;">
+                    <path class="progress-bar-particle-lottie" fill="rgb(255,200,0)" fill-opacity="1"
+                        d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z">
+                    </path>
+                </g>
+            </g>
+            <g transform="matrix(1.8300000429153442,0,0,1.8300000429153442,116.5,108)" opacity="0.39365591842325004"
+                style="display: block;">
+                <path stroke-linecap="butt" stroke-linejoin="miter" fill-opacity="0" stroke-miterlimit="4"
+                    class="progress-bar-particle-lottie" stroke="rgb(255,200,0)" stroke-opacity="1" stroke-width="0"
+                    d=" M0,-50 C27.594999313354492,-50 50,-27.594999313354492 50,0 C50,27.594999313354492 27.594999313354492,50 0,50 C-27.594999313354492,50 -50,27.594999313354492 -50,0 C-50,-27.594999313354492 -27.594999313354492,-50 0,-50z">
+                </path>
+            </g>
+        </g>
+    </svg>
+</template>
